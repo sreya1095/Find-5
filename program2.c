d6cb858c144f0b5afe5333025eb815448a79e0d6
sreya havappanoor <havappanoorsreya@gmail.com>
2025-02-15 14:56:25 +0530

    Update program2.c
    else condition not used

--- program2.c
+++ program2.c
@@ -6,9 +6,11 @@
 int isPrime(int n) {
     if (n <= 1) return 0;
     for (int i = 2; i < n; i++) {
-        if (n / i == 0) return 0;
+        if (n / i == 0) 
+            return 0;
+        else
+            return 1;
     }
-    return 1;
 }
 
 int main() {
